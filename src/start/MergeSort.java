1bf12b6e19620b7c9ecf197e715606bd4a920f20
Мартин Јанев <150262130+MartinJ1st@users.noreply.github.com>
2024-09-26 00:38:14 +0200

    git commit

---
+++ src/start/MergeSort.java
@@ -0,0 +1,95 @@
+package start;
+
+import java.util.Scanner;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        // Create a Scanner object for reading input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for a line of numbers
+        System.out.println("Enter numbers separated by spaces:");
+        String input = scanner.nextLine();
+
+        // Split the input line into an array of strings
+        String[] numberStrings = input.split(" ");
+
+        // Convert the array of strings to an array of integers
+        int[] numbers = new int[numberStrings.length];
+        for (int i = 0; i < numberStrings.length; i++) {
+            numbers[i] = Integer.parseInt(numberStrings[i]);
+        }
+
+        // Perform Merge Sort
+        mergeSort(numbers, 0, numbers.length - 1);
+
+        // Print the sorted array
+        System.out.println("Sorted numbers:");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+
+    // Merge Sort function
+    public static void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+
+            // Sort first half
+            mergeSort(array, left, middle);
+
+            // Sort second half
+            mergeSort(array, middle + 1, right);
+
+            // Merge the two halves
+            merge(array, left, middle, right);
+        }
+    }
+
+    // Merge two subarrays of the array
+    public static void merge(int[] array, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        // Create temporary arrays
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = array[left + i];
+        }
+        for (int i = 0; i < n2; i++) {
+            rightArray[i] = array[middle + 1 + i];
+        }
+
+        // Merge the temp arrays
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of leftArray[], if any
+        while (i < n1) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of rightArray[], if any
+        while (j < n2) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+}
---
+++ src/start/Programa.java
@@ -0,0 +1,7 @@
+package start;
+
+public class Programa {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
+    }
+}
